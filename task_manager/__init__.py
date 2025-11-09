e7465a8dd27b79b4012809c5cdc5fa3877fb8825
Reyanx11 <reyankhan735@gmail.com>
2025-11-09 11:27:07 +0530

    added db to store login details using brcypt

--- task_manager/__init__.py
+++ task_manager/__init__.py
@@ -1,5 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
 
-app = Flask(__name__)
 
-from task_manager import routes
+#extensions
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__)
+
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task_manager.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from task_manager.routes import register_routes
+
+    register_routes(app)
+    return app
--- task_manager/routes.py
+++ task_manager/routes.py
@@ -1,6 +1,37 @@
-from flask import render_template
-from task_manager import app
+from flask import render_template,flash,redirect,url_for
+from task_manager.forms import RegistrationForm, LoginForm
+from task_manager import db,bcrypt
+from task_manager.models import User
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+
+
+def register_routes(app):
+    @app.route('/')
+    def home():
+        return render_template('home.html')
+
+#login route
+    @app.route('/login', methods=['POST','GET'])
+    def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email = form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                flash(f'Login Successful', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Login Unsuccessful, check your username and password', 'danger')
+        return render_template('login.html', title = 'Login', form=form)
+
+#register route
+    @app.route('/register', methods=['POST','GET'])
+    def register():
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash (f'Account created for {form.username.data}', 'success')
+            return redirect(url_for('home'))
+        return render_template('register.html',title = 'Register', form=form)
